be4e273d8ba2718db3dd91c44988a0934f9d6ebf
Pedro Ferreira <devbueno3@gmail.com>
2025-01-15 15:17:38 -0300

    feat: connect with database

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const connection_db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    timezone: '-03:00'
+  }
+);
+
+module.exports = connection_db;
