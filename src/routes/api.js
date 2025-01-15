df4d209fb289681f277d2678c946e71606b43c0e
Pedro Ferreira <devbueno3@gmail.com>
2025-01-15 15:21:20 -0300

    feat: define routes system

---
+++ src/routes/api.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const ContractsController = require('../controllers/contractsController')
+
+//***************** CONTRATOS ******************/
+// ter cuidado com o bind - talvez tenha que usar (req, res) => ContractsController.getAllContracts(req, res)
+router.get('/buscar', ContractsController.getAllContracts)
+
+router.get('/buscar/:id', ContractsController.getContractById)
+
+router.post("/criar", ContractsController.createContract)
+
+router.put('/editar/:id', ContractsController.updateContract)
+
+router.delete('/deletar/:id', ContractsController.deleteContract)
+
+module.exports = router;
