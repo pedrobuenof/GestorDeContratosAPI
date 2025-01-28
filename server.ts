f8c48e1322052a6418437dfbb45862e9c46100c6
Pedro Ferreira <devbueno3@gmail.com>
2025-01-28 00:16:58 -0300

    fix: error cors policy fixed

--- server.ts
+++ server.ts
@@ -1,20 +1,24 @@
 import express from 'express';
 import api from './src/routes/api';
 import connection_db from './config/database';
+import cors from 'cors'
+
+
 require('dotenv').config();
 
 // BANCO DE DADOS
 connection_db.authenticate().then(() => console.log("Conexão com o banco de dados bem-sucedida!")).catch( err => console.error('Erro ao conectar ao banco de dados: ', err));
 connection_db.sync({ force: false }) // Não deleta as tabelas existentes
-  .then(() => {
-    console.log('Tabelas sincronizadas com o banco de dados!');
-  })
-  .catch(err => {
-    console.error('Erro ao sincronizar tabelas: ', err);
-  });
+.then(() => {
+  console.log('Tabelas sincronizadas com o banco de dados!');
+})
+.catch(err => {
+  console.error('Erro ao sincronizar tabelas: ', err);
+});
 
 
 const app = express();
+app.use(cors());
 
 // Middlewares
 app.use(express.json());
